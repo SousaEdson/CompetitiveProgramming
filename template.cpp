1e420a1fc4f3b17187eeffff0889b7ccd93e7995
Edson <edsonsousa734@gmail.com>
2018-03-09 12:18:12 -0300

    update template

--- template.cpp
+++ template.cpp
@@ -1,5 +1,27 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
 #define ff first
 #define ss second
 #define mp make_pair
@@ -12,12 +34,14 @@ using namespace std;
 #define debug(x) cout<<x<<endl
 #define fill(x, y) memset(x, y, sizeof x)
 
+using namespace std;
 typedef long long ll;
 typedef pair<int, int> ii;
 typedef pair<ii, int> iii;
 typedef vector<int>    vi;
 typedef vector<ii>    vii;
-typedef vector<iii>  viii;
+typedef vector<iii>  viii
+///////////////////////////////////////////////////////////////////////////////
 
 int main(){
 		
