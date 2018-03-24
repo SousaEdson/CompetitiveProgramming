78cc88f6b2f2339fad5fd589711e73ac09a3bef2
Edson <edsonsousa734@gmail.com>
2018-03-24 20:28:32 -0300

    add problems

--- UVA/11349.cpp
+++ UVA/11349.cpp
@@ -21,6 +21,44 @@ typedef vector<iii>  viii;
 ///////////////////////////////////////////////////////////////////////////////
 
 int main(){
+	int tc = 1, n;
+	ll m[105][105];	
+	
+	scanf("%d ", &n);
+	
+	while(n--){
+		int sz;
+		scanf("N = %d ", &sz);
 		
+		bool flag = true;
+		
+		fori(i, 0, sz){
+			fori(j, 0, sz){
+				scanf("%lld ", &m[i][j]);				
+				if(m[i][j]<0) flag = false;			
+			}
+		}
+		
+		vector<ll> crs, drs;
+		
+		if(flag){
+			fori(i, 0, sz){
+				fori(j, 0, sz){
+					if(i<=j) crs.pb(m[i][j]);
+				}			
+			}
+			ford(i, sz-1, 0){
+				ford(j, sz-1, 0){
+					if(i>=j) drs.pb(m[i][j]);					
+				}
+			}		
+		}
+				
+		if(!flag) printf("Test #%d: Non-symmetric.\n", tc++);
+		else{
+			printf("Test #%d: ", tc++);
+			printf(crs==drs?"Symmetric.\n":"Non-symmetric.\n");		
+		}	
+	}				
 	return 0;
 }
