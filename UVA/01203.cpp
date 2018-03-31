79f19b3bfbf830ec98f117343032e072e69ca68d
Edson <edsonsousa734@gmail.com>
2018-03-31 16:33:39 -0300

    add problems

--- UVA/01203.cpp
+++ UVA/01203.cpp
@@ -10,7 +10,6 @@
 #define setBit(mask, i) mask = mask | (1 << i)
 #define debug(x) cout<<x<<endl
 #define fill(x, y) memset(x, y, sizeof x)
-#define oo 1<<25
 
 using namespace std;
 typedef long long ll;
@@ -20,39 +19,28 @@ typedef vector<int>    vi;
 typedef vector<ii>    vii;
 typedef vector<iii>  viii;
 ///////////////////////////////////////////////////////////////////////////////
-int m, c, shop[25][25], dp[205][25], sz[25]; //vestuario / preco
-
-int solve(int money, int g){
-	if(money < 0) return -oo;
-	if(g == c) return m - money;
-	if(dp[money][g] != -1) return dp[money][g];	
-	
-	int q = -oo;
-	
-	fori(i, 0, sz[g]){
-		q = max(solve(money - shop[g][i], g+1), q);	
-	}
-	
-	return dp[money][g] = q;
-}
 
 int main(){
-	int tc;
+	multiset<pair<pair<int, int>, int> > s;
+	int id, p;
+	char in[100];
 	
-	scanf("%d", &tc);
-	while(tc--){
-	
-		fill(dp, -1);
-		scanf("%d %d", &m, &c);
-		fori(i, 0, c){			
-			scanf("%d", &sz[i]);
-			fori(j, 0, sz[i]) scanf("%d", &shop[i][j]);			
-		}
-		
-		int q = solve(m, 0);
-		if(q == -oo) printf("no solution\n");
-		else printf("%d\n", q);		
+	while(scanf(" %[^\n]s", in)){		
+		if(in[0] == '#') break;
+		sscanf(in, " Register %d %d", &id, &p);					
+		s.insert(mp(mp(p, id), p));			
 	}
 	
+	int k;
+	scanf("%d", &k);
+	
+	while(k--){
+		auto p = *(s.begin());
+		printf("%d\n", p.ff.ss);		
+		auto i = mp(mp(p.ff.ff+p.ss, p.ff.ss), p.ss);
+		s.erase(s.begin());
+		s.insert(i);
+	}
+		
 	return 0;
 }
